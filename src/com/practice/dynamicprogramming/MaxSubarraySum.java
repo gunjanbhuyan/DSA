a19738c22f0dd51ddccf4ba9b24d20cc2c8bc090
Gunjan Bhuyan <gunjangnn@gmail.com>
2021-11-28 20:07:01 +0530

    MaxSubarraySum

---
+++ src/com/practice/dynamicprogramming/MaxSubarraySum.java
@@ -0,0 +1,33 @@
+package com.practice.dynamicprogramming;
+
+public class MaxSubarraySum {
+	
+	public static int getMaxSubarraySum(int[] arr) {
+		
+		int sum = 0;
+		int minSum = 0;
+		int maxSum = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+			System.out.println("sum : " + sum);
+			
+			minSum = sum < minSum ? sum : minSum;
+			System.out.println("minSum : " + minSum);
+			
+			maxSum = sum - minSum > maxSum ? sum - minSum : maxSum;
+			System.out.println("maxSum : " + maxSum);
+			
+			System.out.println("-----");
+		}
+		
+		return maxSum;
+	}
+
+	public static void main(String[] args) {
+
+		int res = getMaxSubarraySum(new int[] {1, 2, 3, 4, -11, -2, -3, 5, 3, 4});
+		System.out.println(res);
+	}
+
+}
